302884b18086cb24754011921e8706beb2ea2a67
Folkkit Publisher <folkkit-publisher@users.noreply.github.com>
2026-09-09 22:08:18 +0200

    Hosting build from 3b1f6df1355bc9a6d64ca5ffc51d1e140311c854

--- sw.js
+++ sw.js
@@ -1,36 +1,47 @@
 /* eslint-disable no-undef */
-const CACHE_NAME = "folkkit-app-95bc079b750a"
+const CACHE_NAME = "folkkit-app-adda1ef5f16f"
 const PRECACHE_URLS = [
   "/",
-  "/assets/CalculatorPage-CUrwfvZu.js",
+  "/assets/AudioEditorPage-C0vXb6x0.css",
+  "/assets/AudioEditorPage-C5z-jVS2.js",
+  "/assets/CalculatorPage-BYHAW3ik.js",
   "/assets/CalculatorPage-DiVfOUWI.css",
-  "/assets/FileConverterPage-HwqJAkuh.js",
-  "/assets/FileConverterPage-nlcXSCYu.css",
+  "/assets/FileConverterPage-7F0L3Mg-.css",
+  "/assets/FileConverterPage-9nzVePHk.js",
+  "/assets/IconDownload-CdSCj6Ol.js",
+  "/assets/ImageEditorPage-Bk6E_04w.js",
+  "/assets/ImageEditorPage-zvl-SQgZ.css",
   "/assets/PdfEditorPage-Bh-6tEL2.css",
-  "/assets/PdfEditorPage-BxpNK1SQ.js",
+  "/assets/PdfEditorPage-CV6p9OVK.js",
   "/assets/QrDesignerPage-CmBXaxUL.css",
-  "/assets/QrDesignerPage-XZz-QEz5.js",
-  "/assets/WorkspacePage-CE6y_Hw4.js",
+  "/assets/QrDesignerPage-D2rGDAlO.js",
+  "/assets/WorkspacePage-C3cHFbKQ.js",
   "/assets/WorkspacePage-x1HfyxUR.css",
+  "/assets/audioEngine-DwLoGWHb.js",
   "/assets/browser-CWKGGv8Z.js",
-  "/assets/imageFallback-CM0pQRuY.js",
-  "/assets/imageWorker-DaXpIr3k.js",
+  "/assets/detection-BNAOh2CS.js",
+  "/assets/documentEngine-CvuePVqx.js",
+  "/assets/imageEditorWorker-DxkKnnWm.js",
+  "/assets/imageFallback-BnkXsP3v.js",
+  "/assets/imageOperations-D0KBE-jR.js",
+  "/assets/imageWorker-D3sST9Yt.js",
+  "/assets/index-B_sc3gb8.js",
   "/assets/index-BhTHSIFf.js",
-  "/assets/index-CnDizP4q.css",
-  "/assets/index-g4ynK5CV.js",
+  "/assets/index-CVM9K9M-.css",
   "/assets/jpegOrientation-CVoaSQDi.js",
-  "/assets/pdf-AmoQx-ET.js",
+  "/assets/pdf-B2FCOjtE.js",
   "/assets/pdf-lib-BYCLJ2U_.js",
   "/assets/pdfClient-B3O83sLH.js",
   "/assets/pdfStudioWorker-CnwFVfxc.js",
   "/assets/pdfWorker-DVDq7ZnQ.js",
   "/assets/pdfium-RAgkpwfK.wasm",
-  "/assets/qr-BhIuKntD.js",
+  "/assets/profiles-DFty1oBE.js",
+  "/assets/qr-D7sUwM4S.js",
   "/assets/qr-code-styling-MS6qhoCe.js",
   "/assets/qr-preview-DebKZAhx.svg",
   "/assets/qr-reader.worker-DjRSJn4m.js",
   "/assets/qrcode-DgLtAz-0.js",
-  "/assets/workBudgets-C5ZqjDz6.js",
+  "/assets/workBudgets-Do-Hk6pD.js",
   "/favicon.svg",
   "/index.html",
   "/manifest.json",
@@ -39,7 +50,7 @@ const PRECACHE_URLS = [
 const PRECACHE_PATHS = new Set(PRECACHE_URLS)
 const CACHE_PREFIX = 'folkkit-app-'
 const LEGACY_CACHE_NAMES = new Set(['convert-everything-v2'])
-const OPTIONAL_RUNTIME_PATHS = new Set(['/vendor/ffmpeg/ffmpeg-core.js', '/vendor/ffmpeg/ffmpeg-core.wasm'])
+const OPTIONAL_RUNTIME_PATHS = new Set(['/vendor/ffmpeg/ffmpeg-core.js', '/vendor/ffmpeg/ffmpeg-core.wasm', '/vendor/pandoc/pandoc.wasm'])
 const HASHED_ASSET_PATH = /^\/assets\/.+-[A-Za-z0-9_-]{6,}\.(?:css|js|json|svg|png|jpe?g|webp|gif|woff2?)$/
 
 self.addEventListener('install', (event) => {
